22627480d0a61f16c120aea76ca3c766d39e0b0e
Mai Van Manh <maivanmanh@tdtu.edu.vn>
2025-09-30 14:33:29 +0700

    update port to 80 by default

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const os = require("os");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 // SQLite setup
